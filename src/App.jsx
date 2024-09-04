40cbdc0f157dd2820bb3e37ff1e7f9d4dbabe096
Benjamin Li <benjaminli@hotmail.ca>
2024-09-03 20:26:33 -0400

    create repo

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Header from './Header.jsx';
+import './somethins.css';
+
+export default function App() {
+    return (
+        <Header />
+    )
+}
+
---
+++ src/Header.jsx
@@ -0,0 +1,15 @@
+export default function Header() {
+    return(
+        <header>
+            <div class='home'>
+                <a class='home' href='/'>benkyli</a>
+            </div>
+            <div class='navbar'>
+                <a href='/about'>About</a>
+                <a href='/Projects'>Projects</a>
+                <a href='/Resume'>Resume</a>
+                <a href='/contact'>Contact</a>
+            </div>
+        </header>
+    )
+}
