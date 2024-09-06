cd041a6dc977faf75651c2810da2d7b48837da73
Benjamin Li <benjaminli@hotmail.ca>
2024-09-05 22:13:31 -0400

    set navbar to fixed position and included disappearance at small width

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,12 +1,14 @@
 export default function Header() {
     return(
         <nav class='navbar'>
-            <a class='home' href='/'>BL</a>
-            <a class='navbarItem' href='/about'>About</a>
-            <a class='navbarItem' href='/Projects'>Projects</a>
-            <a class='navbarItem' href='/Resume'>Resume</a>
-            <a class='navbarItem' href='/contact'>Contact</a>
+            <a class='home' href='#home'>BL</a>
+            <div class='navbarItems'>
+                <a href='#about'>About</a>
+                <a href='#projects'>Projects</a>
+                <a href='#resume'>Resume</a>
+                <a href='#contact'>Contact</a>
+            </div>
         </nav>
-       
+    //    might need to import each section so we can access them from header
     )
 }
