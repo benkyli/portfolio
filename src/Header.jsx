0112135e194678a29fc813dcafb325750f31e6f2
Benjamin Li <benjaminli@hotmail.ca>
2024-09-10 22:49:52 -0400

    added react state for menu opening

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,14 +1,41 @@
+import { useState } from 'react';
+import { IoMenu, IoCloseOutline } from "react-icons/io5";
+
 export default function Header() {
-    return(
-        <nav class='navbar'>
-            <a class='home' href='#index'>BL</a>
-            <div class='navbarItems'>
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+    
+    return (
+        <div>
+            <nav class='navbar'>
+                <a class='home' href='#index'>BL</a>
+                <div class='navbarItems'>
+                    <a href='#about'>About</a>
+                    <a href='#projects'>Projects</a>
+                    <a href='#resume'>Resume</a>
+                    <a href='#contact'>Contact</a>
+                </div>
+                <div class='menuToggle'> 
+                    <button onClick={toggleMenu}>{isOpen ? <IoCloseOutline /> : <IoMenu />}</button>
+                </div>
+            </nav>
+            {navbarMenu(isOpen)}
+        </div>
+    )
+}
+
+function navbarMenu (isOpen) {
+    if (isOpen) {
+        return (
+            <div class='navbarMenu'>
                 <a href='#about'>About</a>
                 <a href='#projects'>Projects</a>
                 <a href='#resume'>Resume</a>
                 <a href='#contact'>Contact</a>
             </div>
-        </nav>
-    //    might need to import each section so we can access them from header
-    )
+        )
+    }
 }
