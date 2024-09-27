95c1bcce5cbd7599ad379d0257ec67eb42c7e7ab
Benjamin Li <benjaminli@hotmail.ca>
2024-09-27 00:53:56 -0400

    organized containers for side nav-menu and project cards

--- src/About.jsx
+++ src/About.jsx
@@ -2,6 +2,7 @@ export default function About() {
     return (
         <div id='about' className='section'>
             <div className='heading'>About</div>
+            <img src='src/images/headshot.jpg' alt='headshot image' className='max-h-96 brightness-90'></img>
             <p>I took the sunset picture on the homepage when I climbed Mt. Fuji</p>
         </div>
     )
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -2,10 +2,14 @@ import ProjectCard from './ProjectCard.jsx'
 
 // might loop through cards here instead? Probably should do it here, rather than generate the cards prior
 export default function Projects() {
-    return (
+    return(
         <div id='projects' className='section'>
             <h2 className='heading'>Projects</h2>
-            <div className='card-section'>
+            <div className='project-card-container'>
+                <ProjectCard />
+                <ProjectCard />
+                <ProjectCard />
+                <ProjectCard />
                 <ProjectCard />
             </div>
         </div>
