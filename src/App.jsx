cb7fa494576b2b4bd91bc8ed3e985ec4e58edcc7
Benjamin Li <benjaminli@hotmail.ca>
2024-09-29 21:08:24 -0400

    Added a footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import Header from './Header.jsx';
 import Hero from './Hero.jsx'
 import Projects from './Projects.jsx';
 import About from './About.jsx';
-import Contact  from './Contact.jsx';
+import Contact from './Contact.jsx';
+import Footer from './Footer.jsx' 
 import './css/index.css';
 import './css/fonts.css';
 
@@ -14,6 +15,7 @@ export default function App() {
             <Projects />
             <About />
             <Contact />
+            <Footer />
         </div>
     )
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <div className='flex justify-center w-full'>
+            @ 2024 React & Tailwind CSS Portfolio
+        </div>
+    )
+}
