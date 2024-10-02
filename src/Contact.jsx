172c16e81ed01d56e05afdcd2774b40cf02a2b79
Benjamin Li <benjaminli@hotmail.ca>
2024-10-01 22:44:09 -0400

    added links to the social icons

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -3,13 +3,18 @@ import { MdOutlineEmail } from "react-icons/md";
 import ContactForm from './ContactForm.jsx'
 
 export default function Contact() {
+    const openInNewTab  = (url) => {
+        const newTab = window.open(url, '_blank',  'noopener,noreferrer');
+        if (newTab) newTab.opener = null
+    }
+    
     return (
         <div className='contact section'>
             <h2 className='heading'>Contact</h2>
             <ContactForm />
             <div className='socials'>
-                <FaGithub />
-                <FaLinkedin />
+                <button onClick={() => openInNewTab('https://github.com/benkyli')}><FaGithub /></button>
+                <button onClick={() => openInNewTab('https://www.linkedin.com/in/benjamin-li-73b622295')}><FaLinkedin /></button> 
                 <a href='mailto:benjaminli@hotmail.ca'>
                     <MdOutlineEmail />
                 </a>
