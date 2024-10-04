87fcf406110dd7b7b4ad8cc22e8c5546a7ba1a7b
Benjamin Li <benjaminli@hotmail.ca>
2024-10-03 23:05:49 -0400

    created container for contact form

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -16,7 +16,7 @@ export default function Contact() {
                 <button onClick={() => openInNewTab('https://github.com/benkyli')}><FaGithub /></button>
                 <button onClick={() => openInNewTab('https://www.linkedin.com/in/benjamin-li-73b622295')}><FaLinkedin /></button> 
                 <a href='mailto:benjaminli@hotmail.ca'>
-                    <MdOutlineEmail /> benjaminli@hotmail.ca
+                    <MdOutlineEmail /> <p>benjaminli@hotmail.ca</p>
                 </a>
             </div>
         </div>
--- src/ContactForm.jsx
+++ src/ContactForm.jsx
@@ -1,20 +1,20 @@
 export default function ContactForm() {
     return (
-        <form>
+        <form className='contact-form'>
             <p>Contact Form</p>
-            <div>
+            <div className='flex flex-col'>
                 <label for='name'>Full Name</label>
                 <input type='text' placeholder='Enter your name' name='name' required />
             </div>
-            <div>
+            <div className='flex flex-col'>
                 <label for='email'>Email</label>
                 <input type='text' placeholder='Enter your email' name='email' required/>
             </div>
-            <div>
+            <div className='flex flex-col'>
                 <label for='message'>Message</label>
                 <textarea name='message' placeholder='Enter your message' cols='10' rows='2'></textarea>
             </div>
-            <button type='sumbit'>Send Message</button>
+            <button type='submit'>Send Message</button>
         </form>
     )
 }
