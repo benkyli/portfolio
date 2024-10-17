ecad952a5cba1121087fbb7bae09b2792f34e32b
Benjamin Li <benjaminli@hotmail.ca>
2024-10-16 20:45:04 -0400

    added we3forms api request into form

--- src/ContactForm.jsx
+++ src/ContactForm.jsx
@@ -1,26 +1,52 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { DevTool } from '@hookform/devtools';
 
 export default function ContactForm() {
 
     // get methods from useForm and set default values
-    const { register, 
-            control, 
-            handleSubmit, 
-            formState: { errors, isSubmitSuccessful },
-            reset 
-        } = useForm({
-            defaultValues: {
-                contactname: '',
-                contactemail: '',
-                contactmessage: '',
-            }
-        });
+    const { register,
+        control,
+        handleSubmit,
+        formState: { errors, isSubmitSuccessful },
+        reset
+    } = useForm({
+        defaultValues: {
+            contactname: '',
+            contactemail: '',
+            contactmessage: '',
+        }
+    });
 
     // submission request and reset
-    const onSubmit = (data) => {
-        console.log('send data to actual thing')
+    const [isSuccess, setIsSuccess] = useState(false);
+    const [Message, setMessage] = useState("");
+
+    const onSubmit = async (data, e) => {
+        await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+            body: JSON.stringify(data, null, 2),
+        })
+            .then(async (response) => {
+                let json = await response.json();
+                if (json.success) {
+                    setIsSuccess(true);
+                    setMessage(json.message);
+                    e.target.reset();
+                    reset();
+                } else {
+                    setIsSuccess(false);
+                    setMessage(json.message);
+                }
+            })
+            .catch((error) => {
+                setIsSuccess(false);
+                setMessage("Client Error. Please try again");
+            });
     };
 
     useEffect(() => {
@@ -31,51 +57,51 @@ export default function ContactForm() {
 
     return (
         <div>
-        <form className='contact-form' onSubmit={handleSubmit(onSubmit)} onSuccess={() => {alert('Message sent')}} noValidate>
-            <p>Contact Form</p>
-            <div className='form-section'>
-                <label for='contactname'>Full Name</label>
-                <input id='contactname' type='text' placeholder='Enter your name' 
-                    {...register('contactname', { 
-                        required: {
-                            value: true,
-                            message: 'Name required'
-                        },
-                    })} 
-                />
-                <p className='error'>{errors.contactname?.message}</p>
-            </div>
-            <div className='form-section'>
-                <label for='contactemail'>Email</label>
-                <input id='contactemail' type='email' placeholder='Enter your email'
-                    {...register('contactemail', { 
-                        required: {
-                            value: true,
-                            message: 'Email required'
-                        }, 
-                        pattern: {
-                            value: /\S+@\S+\.\S+/,
-                            message: 'Invalid email format',
-                        },
-                    })} 
-                />
-                <p className='error'>{errors.contactemail?.message}</p>
-            </div>
-            <div className='form-section'>
-                <label for='contactmessage'>Message</label>
-                <textarea id='contactmessage' type='text' placeholder='Enter your message' cols='10' rows='2'
-                    {...register('contactmessage', {
-                        required: {
-                            value: true,
-                            message: 'Message cannot be empty'
-                        }
-                    })} 
-                />
-                <p className='error'>{errors.contactmessage?.message}</p>
-            </div>
-            <button type='submit'>Send Message</button>
-        </form>
-        <DevTool control={control}/>
+            <form className='contact-form' onSubmit={handleSubmit(onSubmit)} noValidate>
+                <p>Contact Form</p>
+                <div className='form-section'>
+                    <label htmlFor='contactname'>Full Name</label>
+                    <input id='contactname' type='text' placeholder='Enter your name'
+                        {...register('contactname', {
+                            required: {
+                                value: true,
+                                message: 'Name required'
+                            },
+                        })}
+                    />
+                    <p className='error'>{errors.contactname?.message}</p>
+                </div>
+                <div className='form-section'>
+                    <label htmlFor='contactemail'>Email</label>
+                    <input id='contactemail' type='email' placeholder='Enter your email'
+                        {...register('contactemail', {
+                            required: {
+                                value: true,
+                                message: 'Email required'
+                            },
+                            pattern: {
+                                value: /\S+@\S+\.\S+/,
+                                message: 'Invalid email format',
+                            },
+                        })}
+                    />
+                    <p className='error'>{errors.contactemail?.message}</p>
+                </div>
+                <div className='form-section'>
+                    <label htmlFor='contactmessage'>Message</label>
+                    <textarea id='contactmessage' type='text' placeholder='Enter your message' cols='10' rows='2'
+                        {...register('contactmessage', {
+                            required: {
+                                value: true,
+                                message: 'Message cannot be empty'
+                            }
+                        })}
+                    />
+                    <p className='error'>{errors.contactmessage?.message}</p>
+                </div>
+                <button type='submit'>Send Message</button>
+            </form>
+            <DevTool control={control} />
         </div>
     )
 }
