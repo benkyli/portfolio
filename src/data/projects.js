6e4d22c8f9b8baad1e8097c50a880311ed0b37c6
Benjamin Li <benjaminli@hotmail.ca>
2024-10-22 23:46:53 -0400

    created project data file to be used for props in the project cards

---
+++ src/data/projects.js
@@ -0,0 +1,54 @@
+import { FaYoutube, FaGuitar, FaFolder } from "react-icons/fa";
+import { GiBrain } from "react-icons/gi";
+
+export const ProjectData = [
+    {
+        id: 1,
+        title: "Youtube Live Chat Scroller",
+        image: FaYoutube, // not sure if this is how this will be imported
+        description: "This webapp attempts to recreate the bullet chat scrolling of live stream platforms like Bilibili and Niconico using YouTube's data API. The intended purpose of the app is to be used as an OBS chat overlay.",
+        link: "https://github.com/benkyli/Youtube-Live-Chat-Scroller",
+        tools: [
+            "Flask",
+            "Bootstrap",
+            "Youtube API",
+            "OAuth 2.0"
+        ]
+    },
+    {
+        id: 2,
+        title: "Guitar Chord Identifier",
+        image: FaGuitar,
+        description: "Class identifier that allows you to categorize and classify guitar chords in real time",
+        link: "https://github.com/benkyli/Guitar-Chord-Identifier",
+        tools: [
+            "TensorFlow",
+            "OpenCV",
+            "Python"
+        ]
+    },
+    {
+        id: 3,
+        title: "Portfolio Webssite",
+        image: FaFolder,
+        description: "My portfolio website created using React and Tailwind",
+        link: "https://github.com/benkyli/portfolio",
+        tools: [
+            "React",
+            "react-hook-form",
+            "TailwindCSS",
+            "Web3Forms"
+        ]
+    },
+    {
+        id: 4,
+        title: "Rumination and Stroop Task Thesis",
+        image: GiBrain,
+        description: ,
+        link: ,
+        tools: [
+
+        ]
+    },
+
+];
