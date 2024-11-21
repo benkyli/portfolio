8e058fa3c79bc75307acc13d7d87f70c91cf4232
Benjamin Li <benjaminli@hotmail.ca>
2024-11-20 21:11:20 -0500

    removed finished comments

--- src/ThesisModal.jsx
+++ src/ThesisModal.jsx
@@ -4,13 +4,9 @@ import ProjectCard from './ProjectCard.jsx';
 export default function ThesisModal() {
     return (
         <div id='thesis-modal'>
-            {/* these should show on state click*/}
             {ThesisProjects.map(project =>
                 <ProjectCard key={project.id} {...project} />
             )}
         </div>
     )
-    // steps likely needed for modal. 
-    // 1) add href to modal id instead of page. Perhaps make that a separate page
-    // 2) Add state for modal? 
 }
