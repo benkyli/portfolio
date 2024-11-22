807ed4246543c0a43126e57fb61323aab66996b2
Benjamin Li <benjaminli@hotmail.ca>
2024-11-22 00:25:26 -0500

    set up github pages deploy

--- src/data/about.jsx
+++ src/data/about.jsx
@@ -1,4 +1,4 @@
 export const paragraphs = [
     'While working towards a degree in psychology, neuroscience, and behaviour, I took my first programming course and fell in love with coding. Throughout the remainder of my degree, I focused on computational neuroscience as a way to incorporate my new found passion into my studies.',
-    'Because of my background in science, I have primarily done data analysis, computational modelling, and the coding of experiments. I have also worked on personal projects in my free time to expand my programming knowledge.'
+    'Because of my background in science, I have primarily worked on data analysis, computational modelling, and the coding of experiments. Beyond my studies, I have also done personal projects to expand my programming knowledge.'
 ]
