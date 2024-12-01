eea8eaa296b87211e35b238f0c32013b652174f5
Benjamin Li <benjaminli@hotmail.ca>
2024-12-01 00:43:01 -0500

    fixed mobile sizing for about section

--- src/About.jsx
+++ src/About.jsx
@@ -5,9 +5,9 @@ export default function About() {
     return (
         <div id='about' className='section'>
             <div className='heading'>About</div>
-            <div className='content-container gap-24'>
-                <img src={headshot} alt='headshot image' className='max-h-96 brightness-90'></img>
-                <div className='max-w-screen-lg'>
+            <div className='about-content gap-24'>
+                <img src={headshot} alt='headshot image' className='max-h-80 min-w-52 brightness-90'></img>
+                <div className='about-text'>
                     {paragraphs.map((paragraph, index) =>
                         <p key={index} className='p-4 pt-0 text-3xl'>{paragraph}</p>
                     )}
