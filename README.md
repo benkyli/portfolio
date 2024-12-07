c6eaaf2e9db32c1e3c33e474b7a676dbfe24d463
Benjamin Li <benjaminli@hotmail.ca>
2024-12-07 18:31:55 -0500

    updated readme and changed navbar colour

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # My portfolio website
+#### A simple portfolio website made with Vite React, Tailwind CSS, and Web3Forms. 
+#### Live link: [benjaminli.ca](https://benjaminli.ca)
 
-### Currently a work in progress
+### Image from the Hero section:
+![Hero  section image](/src/images/Herosection.png 'Hero section')
--- src/Hero.jsx
+++ src/Hero.jsx
@@ -2,13 +2,16 @@ import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import { openInNewTab } from './utils/utils.js';
 
 export default function Hero() {
+    const name = 'Benjamin Li';
+    const git_link = 'https://github.com/benkyli';
+    const linkedin_link = 'https://www.linkedin.com/in/benjamin-li-73b622295';
 
     return (
         <div className='hero'>
-            <div id='name'>Benjamin Li</div>
+            <div id='name'>{name}</div>
             <div className='flex gap-10 text-5xl link-hover'>
-                <button onClick={() => openInNewTab('https://github.com/benkyli')}><FaGithub /></button>
-                <button onClick={() => openInNewTab('https://www.linkedin.com/in/benjamin-li-73b622295')}><FaLinkedin /></button>
+                <button onClick={() => openInNewTab({ git_link })}><FaGithub /></button>
+                <button onClick={() => openInNewTab({ linkedin_link })}><FaLinkedin /></button>
             </div>
         </div>
     );
