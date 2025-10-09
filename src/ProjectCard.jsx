3f05e01d254c7f9cede27742c1cf398b05668c5c
Benjamin Li <benjaminli@hotmail.ca>
2025-10-09 19:35:06 -0400

    added hover shadow under project cards instead of colouring the entire slot

--- src/ProjectCard.jsx
+++ src/ProjectCard.jsx
@@ -4,14 +4,14 @@ import PropTypes from 'prop-types';
 export default function ProjectCard(props) {
 
     return (
-        <span onClick={props.modal ? () => props.openModal() : () => openInNewTab(props.link)} className='project-card relative'>
+        <span onClick={props.modal ? () => props.openModal() : () => openInNewTab(props.link)} className='project-card'>
             <span className='flex justify-center text-8xl pb-4'>{props.image}</span>
             <p className='flex justify-center text-2xl'>{props.title}</p>
             <hr />
             <p className='flex p-1 pt-2 pb-3 text-lg grow'>{props.description}</p>
-            <ul className='flex flex-wrap gap-1 pb-3'>
+            <ul className='flex flex-wrap gap-2 pb-3'>
                 {props.tools.map((tool, i) =>
-                    <li className='border-2 rounded-full p-1 bg-neutral-500 flex justify-center items-center' key={i}>{tool}</li>)}
+                    <li className='rounded-full px-2 py-1 bg-neutral-500 flex justify-center items-center' key={i}>{tool}</li>)}
             </ul>
         </span>
     )
